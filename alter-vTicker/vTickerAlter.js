34d9cde2130657eb9a9d3466d1a761c7ec6f1a5d
songshipeng <songshipeng@csii.com.cn>
2015-12-24 16:41:57 +0800

    更新引用js方式

--- alter-vTicker/vTickerAlter.js
+++ alter-vTicker/vTickerAlter.js
@@ -3,212 +3,223 @@
   
 */
 
-(function($){
+(function($) {
 
-  var defaults = {
-    speed: 700,
-    pause: 4000,
-    showItems: 1,
-    mousePause: true,
-    height: 0,
-    animate: true,
-    margin: 0,
-    padding: 0,
-    startPaused: false
-  };
+	var defaults = {
+		speed: 700,
+		pause: 4000,
+		showItems: 1,
+		mousePause: true,
+		height: 0,
+		animate: true,
+		margin: 0,
+		padding: 0,
+		startPaused: false
+	};
 
-  var internal = { 
+	var internal = {
 
-    move: function(state, attribs) {    
-      internal.animate(state, attribs, state.options.direction);
-    },
+		move: function(state, attribs) {
+			internal.animate(state, attribs, state.options.direction);
+		},
 
-    animate: function(state, attribs, dir) {
-      var height = state.itemHeight;
-      var options = state.options;
-      var el = state.element;
-      var obj = el.children('ul');
-      var selector = (dir === 'up') ? 'li:first' : 'li:last';
+		animate: function(state, attribs, dir) {
+			var height = state.itemHeight;
+			var options = state.options;
+			var el = state.element;
+			var obj = el.children('ul');
+			var selector = (dir === 'up') ? 'li:first' : 'li:last';
 
-      el.trigger("vtickeralter.beforeTick");
-      
-      var clone = obj.children(selector).clone(true);
+			el.trigger("vtickeralter.beforeTick");
 
-      if(options.height > 0) height = obj.children('li:first').height();
-      height += (options.margin) + (options.padding*2); // adjust for margins & padding
+			var clone = obj.children(selector).clone(true);
 
-      if(dir==='down') obj.css('top', '-' + height + 'px').prepend(clone);
+			if (options.height > 0) height = obj.children('li:first').height();
+			height += (options.margin) + (options.padding * 2); // adjust for margins & padding
 
-      if(attribs && attribs.animate) {
-        if(state.animating) return;
-        state.animating = true;
-        var opts = (dir === 'up') ? {top: '-=' + height + 'px'} : {top: 0};
-        obj.animate(opts, options.speed, function() {
-            $(obj).children(selector).remove();
-            $(obj).css('top', '0px');
-            state.animating = false;
-            el.trigger("vtickeralter.afterTick");
-          });
-      } else {
-        obj.children(selector).remove();
-        obj.css('top', '0px');
-        el.trigger("vtickeralter.afterTick");
-      }
-      if(dir==='up') clone.appendTo(obj);
-    },
+			if (dir === 'down') obj.css('top', '-' + height + 'px').prepend(clone);
 
-    nextUsePause: function() {
-      var state = $(this).data('state');
-      var options = state.options;
-      if(state.isPaused || methods.isNeed(state)) return;
-      methods.next.call( this);
-    },
+			if (attribs && attribs.animate) {
+				if (state.animating) return;
+				state.animating = true;
+				var opts = (dir === 'up') ? {
+					top: '-=' + height + 'px'
+				} : {
+					top: 0
+				};
+				obj.animate(opts, options.speed, function() {
+					$(obj).children(selector).remove();
+					$(obj).css('top', '0px');
+					state.animating = false;
+					el.trigger("vtickeralter.afterTick");
+				});
+			} else {
+				obj.children(selector).remove();
+				obj.css('top', '0px');
+				el.trigger("vtickeralter.afterTick");
+			}
+			if (dir === 'up') clone.appendTo(obj);
+		},
 
-    startInterval: function() {
-      var state = $(this).data('state');
-      var options = state.options;
-      var initThis = this;
-      state.intervalId = setInterval(function(){ 
-		internal.nextUsePause.call( initThis );
-      }, options.pause);
-    },
+		nextUsePause: function() {
+			var state = $(this).data('state');
+			var options = state.options;
+			if (state.isPaused || methods.isNeed(state)) return;
+			methods.next.call(this);
+		},
 
-    stopInterval: function() {
-      var state = $(this).data('state');
-      if(!state) return;
-      if(state.intervalId) clearInterval(state.intervalId);
-      state.intervalId = undefined;
-    },
+		startInterval: function() {
+			var state = $(this).data('state');
+			var options = state.options;
+			var initThis = this;
+			state.intervalId = setInterval(function() {
+				internal.nextUsePause.call(initThis);
+			}, options.pause);
+		},
 
-    restartInterval: function() {
-      internal.stopInterval.call(this);
-      internal.startInterval.call(this);
-    }
-  };
+		stopInterval: function() {
+			var state = $(this).data('state');
+			if (!state) return;
+			if (state.intervalId) clearInterval(state.intervalId);
+			state.intervalId = undefined;
+		},
 
-  var methods = { 
+		restartInterval: function() {
+			internal.stopInterval.call(this);
+			internal.startInterval.call(this);
+		}
+	};
 
-    init: function(options) {
-      // if init called second time then stop first, then re-init
-      methods.stop.call(this);
-      // init
-      var defaultsClone = jQuery.extend({}, defaults);
-      var options = $.extend(defaultsClone, options);
-      var el = $(this);
-      var state = { 
-        itemCount: el.children('ul').children('li').length,
-        itemHeight: 0,
-        itemMargin: 0,
-        element: el,
-        animating: false,
-        options: options,
-        isPaused: (options.startPaused) ? true : false,
-        pausedByCode: false
-      };
-      $(this).data('state', state);
+	var methods = {
 
-      el.css({overflow: 'hidden', position: 'relative'})
-        .children('ul').css({position: 'absolute', margin: 0, padding: 0})
-        .children('li').css({margin: options.margin, padding: options.padding});
+		init: function(options) {
+			// if init called second time then stop first, then re-init
+			methods.stop.call(this);
+			// init
+			var defaultsClone = jQuery.extend({}, defaults);
+			var options = $.extend(defaultsClone, options);
+			var el = $(this);
+			var state = {
+				itemCount: el.children('ul').children('li').length,
+				itemHeight: 0,
+				itemMargin: 0,
+				element: el,
+				animating: false,
+				options: options,
+				isPaused: (options.startPaused) ? true : false,
+				pausedByCode: false
+			};
+			$(this).data('state', state);
 
-      if(isNaN(options.height) || options.height === 0)
-      {
-        el.children('ul').children('li').each(function(){
-          var current = $(this);
-          if(current.height() > state.itemHeight)
-            state.itemHeight = current.height();
-        });
-        // set the same height on all child elements
-        el.children('ul').children('li').each(function(){
-          var current = $(this);
-          current.height(state.itemHeight);
-        });
-        // set element to total height
-        var box = (options.margin) + (options.padding * 2);
-        el.height(((state.itemHeight + box) * options.showItems) + options.margin);
-      }
-      else
-      {
-        // set the preferred height
-        el.height(options.height);
-      }
+			el.css({
+					overflow: 'hidden',
+					position: 'relative'
+				})
+				.children('ul').css({
+					position: 'absolute',
+					margin: 0,
+					padding: 0
+				})
+				.children('li').css({
+					margin: options.margin,
+					padding: options.padding
+				});
 
-      var initThis = this;
-      if(!options.startPaused) {
-        internal.startInterval.call( initThis );
-      }
+			if (isNaN(options.height) || options.height === 0) {
+				el.children('ul').children('li').each(function() {
+					var current = $(this);
+					if (current.height() > state.itemHeight)
+						state.itemHeight = current.height();
+				});
+				// set the same height on all child elements
+				el.children('ul').children('li').each(function() {
+					var current = $(this);
+					current.height(state.itemHeight);
+				});
+				// set element to total height
+				var box = (options.margin) + (options.padding * 2);
+				el.height(((state.itemHeight + box) * options.showItems) + options.margin);
+			} else {
+				// set the preferred height
+				el.height(options.height);
+			}
 
-      if(options.mousePause)
-      {
-        el.bind("mouseenter", function () {
-          //if the automatic scroll is paused, don't change that.
-          if (state.isPaused === true) return; 
-          state.pausedByCode = true; 
-          // stop interval
-          internal.stopInterval.call( initThis );
-          methods.pause.call( initThis, true );
-        }).bind("mouseleave", function () {
-          //if the automatic scroll is paused, don't change that.
-          if (state.isPaused === true && !state.pausedByCode) return;
-          state.pausedByCode = false; 
-          methods.pause.call(initThis, false);
-          // restart interval
-          internal.startInterval.call( initThis );
-        });
-      }
-    },
+			var initThis = this;
+			if (!options.startPaused) {
+				internal.startInterval.call(initThis);
+			}
 
-    pause: function(pauseState) {
-      var state = $(this).data('state');
-      if(!state) return undefined;
-      if(methods.isNeed(state)) return false;
-      state.isPaused = pauseState;
-      var el = state.element;
-      if(pauseState) {
-        $(this).addClass('paused');
-        el.trigger("vtickeralter.pause");
-      }
-      else {
-        $(this).removeClass('paused');
-        el.trigger("vtickeralter.resume");
-      }
-    },
-	
-    next: function(attribs) {
-      var state = $(this).data('state');
-      if(!state) return undefined;
-      if(state.animating || methods.isNeed(state)) return false;
-      internal.restartInterval.call( this );
-      internal.move(state, {animate:state.options.animate}); 
-    },
+			if (options.mousePause) {
+				el.bind("mouseenter", function() {
+					//if the automatic scroll is paused, don't change that.
+					if (state.isPaused === true) return;
+					state.pausedByCode = true;
+					// stop interval
+					internal.stopInterval.call(initThis);
+					methods.pause.call(initThis, true);
+				}).bind("mouseleave", function() {
+					//if the automatic scroll is paused, don't change that.
+					if (state.isPaused === true && !state.pausedByCode) return;
+					state.pausedByCode = false;
+					methods.pause.call(initThis, false);
+					// restart interval
+					internal.startInterval.call(initThis);
+				});
+			}
+		},
 
-    stop: function() {
-      var state = $(this).data('state');
-      if(!state) return undefined;
-      internal.stopInterval.call( this );
-    },
+		pause: function(pauseState) {
+			var state = $(this).data('state');
+			if (!state) return undefined;
+			if (methods.isNeed(state)) return false;
+			state.isPaused = pauseState;
+			var el = state.element;
+			if (pauseState) {
+				$(this).addClass('paused');
+				el.trigger("vtickeralter.pause");
+			} else {
+				$(this).removeClass('paused');
+				el.trigger("vtickeralter.resume");
+			}
+		},
 
-    remove: function() {
-      var state = $(this).data('state');
-      if(!state) return undefined;
-      internal.stopInterval.call( this );
-      var el = state.element;
-      el.unbind();
-      el.remove();
-    },
-	
-	isNeed: function(state){
-		return state && state.itemCount < state.options.showItems;
-	}
-  };
- 
-  $.fn.vTickerAlter = function( method ) {
-    if ( methods[method] ) {
-      return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
-    } else if ( typeof method === 'object' || ! method ) {
-      return methods.init.apply( this, arguments );
-    } else {
-      $.error( 'Method ' +  method + ' does not exist on jQuery.vTickerAlter' );
-    }    
-  };
+		next: function(attribs) {
+			var state = $(this).data('state');
+			if (!state) return undefined;
+			if (state.animating || methods.isNeed(state)) return false;
+			internal.restartInterval.call(this);
+			internal.move(state, {
+				animate: state.options.animate
+			});
+		},
+
+		stop: function() {
+			var state = $(this).data('state');
+			if (!state) return undefined;
+			internal.stopInterval.call(this);
+		},
+
+		remove: function() {
+			var state = $(this).data('state');
+			if (!state) return undefined;
+			internal.stopInterval.call(this);
+			var el = state.element;
+			el.unbind();
+			el.remove();
+		},
+
+		isNeed: function(state) {
+			return state && state.itemCount < state.options.showItems;
+		}
+	};
+
+	$.fn.vTickerAlter = function(method) {
+		if (methods[method]) {
+			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+		} else if (typeof method === 'object' || !method) {
+			return methods.init.apply(this, arguments);
+		} else {
+			$.error('Method ' + method + ' does not exist on jQuery.vTickerAlter');
+		}
+	};
 })(jQuery);
